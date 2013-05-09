7456a5aeba3794fffc3a2db176808cad0fdc7041
Liu Wei <liuweiky@qq.com>
2013-05-09 17:45:37 +0800

    修改cache目录

--- mbot/settings.py
+++ mbot/settings.py
@@ -7,6 +7,6 @@ DEFAULT_ITEM_CLASS = 'mbot.items.MovieInfo'
 ITEM_PIPELINES = ['mbot.pipelines.SQLiteStorePipeline']
 DOWNLOAD_DELAY = 0.07
 HTTPCACHE_ENABLED = True
-HTTPCACHE_DIR = 'C:/scrapy-cache/filedbm'
+HTTPCACHE_DIR = 'scrapy-cache'
 LOG_LEVEL = "INFO"
 DOWNLOAD_TIMEOUT = 5
